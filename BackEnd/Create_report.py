36f6f98ec04614344888827a29cc07076d6172c1
Sapir <sapirnahum9@gmail.com>
2021-04-02 14:07:45 +0300

    update report and rate normalization

--- BackEnd/Create_report.py
+++ BackEnd/Create_report.py
@@ -52,8 +52,10 @@ class create_report():
                            'Score':scores,
                            'topics':topics,
                            'synonyms':synon})
+        print(df)
+
         writer = pd.ExcelWriter("files/report.xlsx", engine='xlsxwriter')
-        df.to_excel(writer, sheet_name=book_id, startrow=0, header=False)
+        df.to_excel(writer, sheet_name=book_id, startrow=1, header=False)
 
         workbook = writer.book
         worksheet = writer.sheets[book_id]
@@ -62,12 +64,25 @@ class create_report():
             'bold': True,
             'text_wrap': True,
             'valign': 'top',
-            'fg_color': '#D7E4BC',
+            'fg_color': '#000000',
+            'font_color': '#FFFFFF',
             'border': 1})
+        book_format = workbook.add_format({
+            'bold': True,
+            'font_color': '#3300FF'})
 
-        # Write the column headers with the defined format.
+        # # Write the column headers with the defined format.
         for col_num, value in enumerate(df.columns.values):
             worksheet.write(0, col_num+1, value, header_format)
+        worksheet.set_row(1,None,book_format)
 
+        for i, col in enumerate(df.columns):
+            # find length of column i
+            column_len = df[col].astype(str).str.len().max()
+            # Setting the length if the column header is larger
+            # than the max column value length
+            column_len = max(column_len, len(col)) + 2
+            # set the column length
+            worksheet.set_column(i+1,i+1, column_len)
         # Close the Pandas Excel writer and output the Excel file.
         writer.save()
--- BackEnd/Rate_books.py
+++ BackEnd/Rate_books.py
@@ -42,8 +42,8 @@ class rate_books():
         topics_list1 = es.get_book_topics(book1)
         topics_list2 = es.get_book_topics(book2)
         score = 0
-        threshold = 0.80     # if needed
-        combineList= len(topics_list1) + len(topics_list2)
+        threshold = 0.50     # if needed
+        counter = 0
         for key in topics_list1:
             for word in topics_list2:
                 x = wordnet.synsets(key)
@@ -51,68 +51,9 @@ class rate_books():
                 if not x or not y:
                     continue
                 s = x[0].wup_similarity(y[0])
-                if s is not None:
+                if s is not None and s>threshold:
+                    counter += 1
                     score += s
-                # print(key,", ",word," = ",s)
-        return score/combineList
-
-
-
-
-
-
-
-#     def rate2words(self,word1,word2):
-#         # load the language model
-#         nlp = spacy.load('en_core_web_md')
-#
-#         # convert the strings to spaCy Token objects
-#         token1 = nlp(word1)[0]
-#         token2 = nlp(word2)[0]
-#         # compute word similarity
-#         score = token1.similarity(token2)
-#
-#         print(word1,", ",word2," = ",score)
-#         return score
-#
-#
-#     def get_rate(self,book1,book2):
-#         es = elasticsearch()
-#         topics_list1 = es.get_book_topics(book1)
-#         topics_list2 = es.get_book_topics(book2)
-#         score = 0
-#         threshold = 0.80     # if needed
-#         combineList= list(set(topics_list1) | set(topics_list2))
-#         for key in topics_list1:
-#             for word in topics_list2:
-# #               res = self.cosdis(self.word2vec(word), self.word2vec(key))
-#                 res = self.rate2words(key,word)
-#                 if res > threshold:
-#                     score +=  res
-#
-#         return score/len(combineList)
-#
-
-#
-#
-#     def word2vec(self,word):
-#         from collections import Counter
-#         from math import sqrt
-#
-#         # count the characters in word
-#         cw = Counter(word)
-#         # precomputes a set of the different characters
-#         sw = set(cw)
-#         # precomputes the "length" of the word vector
-#         lw = sqrt(sum(c*c for c in cw.values()))
-#
-#         # return a tuple
-#         return cw, sw, lw
-#
-#     def cosdis(self,v1, v2):
-#         # which characters are common to the two words?
-#         common = v1[1].intersection(v2[1])
-#         # by definition of cosine distance we have
-#         return sum(v1[0][ch]*v2[0][ch] for ch in common)/v1[2]/v2[2]
-#
-#
+        if counter!=0:
+            score=score/counter
+        return score
